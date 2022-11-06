0e57a5a972b8e82ce01816f9ba170d4f6264a034
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-11-06 14:03:41 +0300

    feat: delete warning (error) and add hook: usestate

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import './scss/app.scss'
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
     
       <Header />
      
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             
             <Categories />
             {/* Еще один способ вызванить функцию это: 
@@ -23,8 +23,8 @@ function App() {
            <Sort />
 
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
               <PizzaBlock title="Мексиканская" price={100} />
               <PizzaBlock title="Aligator" price={500} />
               <PizzaBlock />
