54034cffb3ad5ebb7b21242fe668d2ff780842a9
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-11-16 15:32:54 +0300

    feat: add fake-loading

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Categories from './Components/Categories'
 
 function App() {
   const [items, setItems] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
 
   React.useEffect(() => {
     fetch('https://634812fbdb76843976b9b35d.mockapi.io/Collections')
@@ -16,7 +17,8 @@ function App() {
       return res.json();
     })
     .then((arr) => {
-      setItems(arr)
+      setItems(arr)           // Отрендери пиццы
+      setIsLoading(false)     // Загрузка завершилась
     });
   }, [])
 
@@ -32,9 +34,18 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((object) => (
-              <Skeleton key={object.id} {...object} />
-            ))}
+            {
+              /* 1. Если у нас сейчас идет загрузка, то мы создаем фейковых массив: [...new Array()] 
+              Внутрь пишем сколько необходим кол-во элементов и замени все undefined на Скелетон, 
+              а если загрузка завершилась реднери пицца блок
+              
+              Каждый раз когда рендерится какой-то список обязательно указывай ключ. 
+              
+              Нижнее подчеркивание необходим был для того, чтобы js не ругался, так как в нем нет значений
+              */
+              isLoading ? [...new Array(6)].map(( _, index) => <Skeleton key={index}/>)
+                        : items.map(object => <PizzaBlock key = {object.id} {...object} />)
+            }
           </div>
         </div>
       </div>
