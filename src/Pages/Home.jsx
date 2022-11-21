9385dfcbc1a3facf9d8c1f3b83d3f2ceca2361fb
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-11-21 10:47:36 +0300

    feat: add library pagination and use them

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,20 +4,21 @@ import PizzaBlock from '../Components/PizzaBlock';
 import Skeleton from '../Components/PizzaBlock/Skeleton';
 import Categories from '../Components/Categories';
 import { Sort } from '../Components/Sort';
+import Pagination from '../Components/Pagination';
 
 const Home = ( {searchValue} ) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryID, setCategoryID] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(0)
   const [sortType, setSortType] = React.useState({ name: 'популярности', sortProperty: 'rating' });
 
   React.useEffect(() => {
     const sortBy = sortType.sortProperty.replace('-', "");
     const order = sortType.sortProperty.includes('-') ? "asc" : "desc";
     const category = categoryID > 0 ? `category=${categoryID}` : '';
-    const search = searchValue > 0 ? `search=${searchValue}` : '';
     
-    fetch(`https://634812fbdb76843976b9b35d.mockapi.io/Collections?${category}&sortBy=${sortBy}&order=${order}${search}`,)
+    fetch(`https://634812fbdb76843976b9b35d.mockapi.io/Collections?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}`,)
       .then((res) => {
         return res.json();
       })
@@ -26,7 +27,7 @@ const Home = ( {searchValue} ) => {
         setIsLoading(false); 
       });
     window.scrollTo(0, 0);
-  }, [categoryID, sortType, searchValue]);
+  }, [categoryID, sortType, currentPage]);
 
   const fakePizza = [...new Array(6)].map((_, index) => <Skeleton key={index} />)
   const objectMap = items.map((object) => <PizzaBlock key={object.id} {...object} />)
@@ -43,6 +44,8 @@ const Home = ( {searchValue} ) => {
         {isLoading ? fakePizza : objectMap }
         
       </div>
+
+      <Pagination onChangePage = {(number) => setCurrentPage(number)}/>
     </div>
   );
 };
