f5f0d68614b8580cad2b673d2de3e3f48f8e4aaf
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-11-28 00:12:52 +0300

    feat: await in axios and remove then

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,65 +7,70 @@ import { list, Sort } from '../Components/Sort';
 import Pagination from '../Components/Pagination';
 import { SearchContext } from '../App';
 
-import { useSelector, useDispatch } from 'react-redux'
-import { setCategoryId, setCurrentPage, setFilter } from '../Components/Redux/slices/filterSlice'
+import { useSelector, useDispatch } from 'react-redux';
+import { setCategoryId, setCurrentPage, setFilter } from '../Components/Redux/slices/filterSlice';
 
 import axios from 'axios';
 
-import qs from 'qs'
+import qs from 'qs';
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const isSearch = React.useRef(false);
   const isMounted = React.useRef(false);
 
-  const categoryID = useSelector(state => state.filter.categoryID);
-  const sortType = useSelector((state) => state.filter.sort.sortProperty)
-  const currentPage = useSelector((state) => state.filter.currentPage)
+  const categoryID = useSelector((state) => state.filter.categoryID);
+  const sortType = useSelector((state) => state.filter.sort.sortProperty);
+  const currentPage = useSelector((state) => state.filter.currentPage);
 
-  const {searchValue} = React.useContext(SearchContext)
+  const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   const onClickCategory = (id) => {
-    dispatch(setCategoryId(id))
-  } 
+    dispatch(setCategoryId(id));
+  };
 
   const onChangePage = (number) => {
-    dispatch(setCurrentPage(number))
-  }
+    dispatch(setCurrentPage(number));
+  };
 
   const fetchPizzas = async () => {
-      const sortBy = sortType.replace('-', "");
-      const order = sortType.includes('-') ? "asc" : "desc";
-      const category = categoryID > 0 ? `category=${categoryID}` : '';
-      const search = searchValue > 0 ? `search=${searchValue}` : '';
-      
-      await axios.get(`https://634812fbdb76843976b9b35d.mockapi.io/Collections?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
-          .then((response) => {
-          // .then(async (response) => {
-              setItems(response.data); 
-              setIsLoading(false); 
-              // console.log(1)
-          })
-        // console.log(2)
-      }
+    const sortBy = sortType.replace('-', '');
+    const order = sortType.includes('-') ? 'asc' : 'desc';
+    const category = categoryID > 0 ? `category=${categoryID}` : '';
+    const search = searchValue > 0 ? `search=${searchValue}` : '';
 
-      
+    //await axios.get(`https://634812fbdb76843976b9b35d.mockapi.io/Collections?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
+    // .then((response) => {
+    // .then(async (response) => {
+    // setItems(response.data);
+    // setIsLoading(Sfalse);
+    // console.log(1)
+    // })
+    // console.log(2)
+    // }
 
-  React.useEffect(()=>{
+    const response = await axios.get(
+      `https://634812fbdb76843976b9b35d.mockapi.io/Collections?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+    );
+    setItems(response.data);
+    setIsLoading(false);
+  };
+
+  React.useEffect(() => {
     if (isMounted.current) {
       const queryString = qs.stringify({
-        sortProperty : sortType,
+        sortProperty: sortType,
         categoryID,
         currentPage,
-      })
-      navigate(`?${queryString}`)
+      });
+      navigate(`?${queryString}`);
     }
     isMounted.current = true;
-  }, [categoryID, sortType, currentPage])
+  }, [categoryID, sortType, currentPage]);
 
   React.useEffect(() => {
     if (window.location.search) {
@@ -73,9 +78,7 @@ const Home = () => {
       // Возьмем реальный sort, который у нас есть и перадим в редакс
       const sort = list.find((obj) => obj.sortProperty === params.sortProperty);
 
-      dispatch(
-        setFilter( { ...params, sort } ),
-      );
+      dispatch(setFilter({ ...params, sort }));
       isSearch.current = true;
     }
   }, []);
@@ -86,31 +89,28 @@ const Home = () => {
       fetchPizzas();
     }
     isSearch.current = false;
-  }, [categoryID, sortType, searchValue, currentPage])
+  }, [categoryID, sortType, searchValue, currentPage]);
 
+  const fakePizza = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+  const objectMap = items
+    .filter((obj) => {
+      if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+        return true;
+      }
+      return false;
+    })
+    .map((object) => <PizzaBlock key={object.id} {...object} />);
 
-
-  const fakePizza = [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-  const objectMap = items.filter(obj => {
-    if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-      return true
-    }
-    return false
-  }).map((object) => <PizzaBlock key={object.id} {...object} />)
-  
   return (
     <div className="container">
       <div className="content__top">
         <Categories value={categoryID} onClickCategory={onClickCategory} />
-        <Sort/>
+        <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading ? fakePizza : objectMap }
+      <div className="content__items">{isLoading ? fakePizza : objectMap}</div>
 
-      </div>
-
-      <Pagination currentPage={currentPage} onChangePage={onChangePage}/>
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
 };
