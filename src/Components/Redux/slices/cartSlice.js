30547cea18d077e70ae1cda8ffa2c6f183296992
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-11-27 16:39:46 +0300

    feat: improve counts

--- src/Components/Redux/slices/cartSlice.js
+++ src/Components/Redux/slices/cartSlice.js
@@ -3,7 +3,6 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
     // Общая сумма
     totalPrice: 0,
-    price: 0,
     // Товары
     items: []
 };
@@ -12,10 +11,29 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addItem(state, action) {
+    /* addItem(state, action) {
       state.items.push(action.payload)
       state.totalPrice = state.items.reduce((sum, obj) => {
         return obj.price + sum
+      }, 0)
+    }, */
+
+    addItem(state, action) {
+    // Найти мне этот объект в массиве
+      const findItem = state.items.find(obj => obj.id === action.payload)
+    // Если он есть, увеличь его на ++, на 1
+      if (findItem == true) {
+        findItem.count++;
+      } else {
+    // Если он не нашелся, то мы добавляем новый объект
+      state.items.push({
+        ...action.payload, 
+        count: 1
+      })
+    } 
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum
       }, 0)
     },
     removeItem(state, action) {
